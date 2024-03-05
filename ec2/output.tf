7427cd584badd7283f86dd8210ac1e1014d11c68
hitika.bhandari@neosoftmail.com <hitika.bhandari@neosoftmail.com>
2024-03-05 15:30:48 +0530

    op

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,9 +1,9 @@
 output "Private_instance_id" {
-  value       = aws_instance.Private_ec2.id
+  value = aws_instance.Private_ec2.id
 }
 
 output "Public_instance_id" {
-  value       = aws_instance.Public_ec2.id
+  value = aws_instance.Public_ec2.id
 }
 
 output "ec2instance" {
